e540d152cccba9a3f5e98dfff555d1d60cfd8fc4
Sebastian <sebastian.zawolanski@gmail.com>
2023-08-26 15:11:35 +0200

    Update the browserlist and add the page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
 import Layout from 'components/Layout';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyled from 'styles/globalStyles';
 import theme from 'styles/theme/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyled />
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </ThemeProvider>
+  <>
+    <Head>
+      <title>sunnyside</title>
+    </Head>
+    <ThemeProvider theme={theme}>
+      <GlobalStyled />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  </>
 );
 
 export default MyApp;
